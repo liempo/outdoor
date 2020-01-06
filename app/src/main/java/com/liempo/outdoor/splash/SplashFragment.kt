01b1fac0d0e20cbd12ca7b23f1cf872cf504b8d4
Liempo <gonzalesalec@gmail.com>
2020-01-07 02:46:02 +0800

    Added permission checks

--- app/src/main/java/com/liempo/outdoor/splash/SplashFragment.kt
+++ app/src/main/java/com/liempo/outdoor/splash/SplashFragment.kt
@@ -1,6 +1,10 @@
 package com.liempo.outdoor.splash
 
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +12,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
+import timber.log.Timber
 
 import com.liempo.outdoor.R
 
@@ -45,11 +50,48 @@ class SplashFragment : Fragment() {
                             .createSignInIntentBuilder()
                             .setAvailableProviders(providers)
                             .build(),
-                    REQUEST_SIGN_IN)
+                    RC_SIGN_IN)
+        }
+
+        // Ask for permissions for the whole app if not granted
+        if (APP_PERMISSIONS.all { activity?.checkSelfPermission(it) ==
+                    PackageManager.PERMISSION_GRANTED }.not()) {
+            requestPermissions(APP_PERMISSIONS, RC_APP_PERMISSIONS)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            Timber.d("User logged in: ${auth.currentUser?.phoneNumber}")
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == RC_APP_PERMISSIONS && grantResults.
+                        any { it != PackageManager.PERMISSION_GRANTED }) {
+            AlertDialog.Builder(activity)
+                    .setMessage(R.string.error_missing_permissions)
+                    .setPositiveButton(android.R.string.ok) { _, _ ->
+                        activity?.finish()
+                    }.show()
         }
     }
 
     companion object {
-        private const val REQUEST_SIGN_IN = 2934
+        private const val RC_SIGN_IN = 2934
+
+        private const val RC_APP_PERMISSIONS = 5123
+        private val APP_PERMISSIONS = arrayOf(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.SEND_SMS,
+                Manifest.permission.CAMERA
+        )
     }
 }
