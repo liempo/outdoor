3435af3ab7533ecd994ad52f97170a9604c3d8b3
Liempo <gonzalesalec@gmail.com>
2020-01-07 01:32:49 +0800

    Added a splash screen

---
+++ app/src/main/java/com/liempo/outdoor/splash/SplashFragment.kt
@@ -0,0 +1,21 @@
+package com.liempo.outdoor.splash
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.liempo.outdoor.R
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.fragment_splash,
+                    container, false)
+
+
+}
