9c2f9e43e8f4532d14949818889211f799b0bdf0
Liempo <gonzalesalec@gmail.com>
2020-01-07 00:24:24 +0800

    Initial commit

---
+++ app/src/main/java/com/liempo/outdoor/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.liempo.outdoor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
