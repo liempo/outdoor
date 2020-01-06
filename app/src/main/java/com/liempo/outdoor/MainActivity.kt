fe78cb905eb084b7ca7919b39d6e0d88082f7ead
Liempo <gonzalesalec@gmail.com>
2020-01-07 03:27:29 +0800

    Planted a debug tree in main activity

--- app/src/main/java/com/liempo/outdoor/MainActivity.kt
+++ app/src/main/java/com/liempo/outdoor/MainActivity.kt
@@ -3,12 +3,16 @@ package com.liempo.outdoor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mapbox.mapboxsdk.Mapbox
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Initialize Mapbox for Mapbox stuff
         Mapbox.getInstance(this, BuildConfig.MapboxApiKey)
+
+        // Plant a debug Tree here
+        Timber.plant(Timber.DebugTree())
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
