46692564e8f5fb3d7998d1beb78e53ab8203f1c3
Liempo <gonzalesalec@gmail.com>
2020-01-07 01:50:20 +0800

    Added Firebase Auth UI

--- app/src/main/java/com/liempo/outdoor/splash/SplashFragment.kt
+++ app/src/main/java/com/liempo/outdoor/splash/SplashFragment.kt
@@ -6,10 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 
 import com.liempo.outdoor.R
 
 class SplashFragment : Fragment() {
+
+    // Will be used to check if user is logged in
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize firebase auth
+        auth = FirebaseAuth.getInstance()
+    }
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -17,5 +29,27 @@ class SplashFragment : Fragment() {
             inflater.inflate(R.layout.fragment_splash,
                     container, false)
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        // Check if not logged in
+        if (auth.currentUser == null) {
+            // List of log in providers (log in with phone only)
+            val providers = arrayListOf(
+                    AuthUI.IdpConfig.PhoneBuilder().build()
+            )
+
+            // Start Firebase UI authentication
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    REQUEST_SIGN_IN)
+        }
+    }
+
+    companion object {
+        private const val REQUEST_SIGN_IN = 2934
+    }
 }
